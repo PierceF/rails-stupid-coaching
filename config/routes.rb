49347e6a8462b66f9324a71e1c1d63d3c38a90dd
Pierce Franklin <piush_r@hotmail.com>
2019-06-24 15:10:18 +0900

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Verb, to: 'controller#method'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
 end
+
+# link_to "Ask", ask_path
+# <a href="/ask"
